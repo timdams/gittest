dc04a7ab4a5242f53cad3c9cf7d8f9ba82b94738
Tim Dams <tim.dams@ap.be>
2020-02-10 11:30:28 +0100

    Eerste deel is klaar

--- Deel1/Program.cs
+++ Deel1/Program.cs
@@ -7,6 +7,7 @@ namespace Deel1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world 2");
         }
     }
 }
